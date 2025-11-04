ee092e30d202a8b4851ed6e95f06969bca4f091c
yanxiaolu <yanxiaolu@gmail.com>
2025-11-04 22:56:08 +0800

    添加数据迁移和内容类型

--- AmazData.Module.Mqtt/Startup.cs
+++ AmazData.Module.Mqtt/Startup.cs
@@ -1,6 +1,10 @@
+using AmazData.Module.Mqtt.Migrations;
+using AmazData.Module.Mqtt.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.ContentManagement;
+using OrchardCore.Data.Migration;
 using OrchardCore.Modules;
 
 namespace AmazData.Module.Mqtt;
@@ -9,6 +13,10 @@ public sealed class Startup : StartupBase
 {
     public override void ConfigureServices(IServiceCollection services)
     {
+        services.AddDataMigration<MqttMigrations>();
+        services.AddContentPart<BrokerPart>();
+        services.AddContentPart<TopicPart>();
+        services.AddContentPart<DataRecordPart>();
     }
 
     public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
