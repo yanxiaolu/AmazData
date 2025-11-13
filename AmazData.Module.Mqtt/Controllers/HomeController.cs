8e87e04504634bbab813052ede2b5680682a2143
yanxiaolu <yanxiaolu@gmail.com>
2025-11-14 00:45:54 +0800

    连接mqtt connect Controller

--- AmazData.Module.Mqtt/Controllers/HomeController.cs
+++ AmazData.Module.Mqtt/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using OrchardCore.ContentManagement;
 using System.Threading.Tasks;
 using AmazData.Module.Mqtt.Services;
+using OrchardCore.DisplayManagement.Notify;
+using Microsoft.AspNetCore.Mvc.Localization;
 
 namespace AmazData.Module.Mqtt.Controllers
 {
@@ -10,15 +12,20 @@ namespace AmazData.Module.Mqtt.Controllers
         private readonly IContentManager _contentManager;
         private readonly IMqttConnectionManager _mqttConnectionManager;
         private readonly IMqttOptionsBuilderService _mqttOptionsBuilderService;
-
+        private readonly INotifier _notifier;
+        private readonly IHtmlLocalizer<SubscriptionController> _localizer;
         public HomeController(
             IContentManager contentManager,
             IMqttConnectionManager mqttConnectionManager,
-            IMqttOptionsBuilderService mqttOptionsBuilderService)
+            IMqttOptionsBuilderService mqttOptionsBuilderService,
+            INotifier notifier,
+            IHtmlLocalizer<SubscriptionController> localizer)
         {
             _contentManager = contentManager;
             _mqttConnectionManager = mqttConnectionManager;
             _mqttOptionsBuilderService = mqttOptionsBuilderService;
+            _notifier = notifier;
+            _localizer = localizer;
         }
 
         public async Task<IActionResult> Index(string brokerId)
@@ -36,8 +43,8 @@ namespace AmazData.Module.Mqtt.Controllers
             {
                 await _mqttConnectionManager.ConnectAsync(brokerId, options);
             }
-
-            return View(contentItem);
+            await _notifier.SuccessAsync(_localizer["Broker Connected successfully."]);
+            return RedirectToAction("List", "Admin", new { area = "OrchardCore.Contents", contentTypeId = "Broker" });
         }
     }
 }
