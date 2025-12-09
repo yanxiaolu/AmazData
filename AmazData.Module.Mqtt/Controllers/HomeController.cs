dd8f8b975f6186dfdecdda4ca63f35d0240ff7cf
yanxiaolu <yanxiaolu@gmail.com>
2025-12-10 02:43:14 +0800

    refactor: 移除未使用的 IMqttOptionsBuilderService 依赖

--- AmazData.Module.Mqtt/Controllers/HomeController.cs
+++ AmazData.Module.Mqtt/Controllers/HomeController.cs
@@ -12,14 +12,12 @@ namespace AmazData.Module.Mqtt.Controllers
     {
         private readonly IContentManager _contentManager;
         private readonly IMqttConnectionManager _mqttConnectionManager;
-        private readonly IMqttOptionsBuilderService _mqttOptionsBuilderService;
         private readonly INotifier _notifier;
         private readonly IHtmlLocalizer<HomeController> _localizer;
         private readonly IBrokerService _brokerService;
         public HomeController(
     IContentManager contentManager,
     IMqttConnectionManager mqttConnectionManager,
-    IMqttOptionsBuilderService mqttOptionsBuilderService,
     INotifier notifier,
     IHtmlLocalizer<HomeController> localizer,
     IBrokerService brokerService
@@ -27,7 +25,6 @@ namespace AmazData.Module.Mqtt.Controllers
         {
             _contentManager = contentManager;
             _mqttConnectionManager = mqttConnectionManager;
-            _mqttOptionsBuilderService = mqttOptionsBuilderService;
             _notifier = notifier;
             _localizer = localizer;
             _brokerService = brokerService;
